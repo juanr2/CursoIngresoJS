f7b52a4671801dae502551be48b78c2781650741
juanr2 <roa_juan@hotmail.com>
2017-02-19 13:30:59 -0300

    cam

--- 5-InstruccionWhile/jsIteraciones (6).js
+++ 5-InstruccionWhile/jsIteraciones (6).js
@@ -1,5 +1,27 @@
 function Mostrar()
 {
+    var numeros=0;
+    parseInt("numeros");
+    var cantidadacumulada;
+    var promedio;
+    
+
+
+    while(numeros<5)
+
+    {
+        numeros++;
+        numeros=prompt("ingrese un numero");
+        console.log(numeros);
+
+    }
+        cantidadacumulada+=numeros;
+        promedio=cantidadacumulada/5;
+
+
+
+    document.getElementById("suma").value=cantidadacumulada;
+    document.getElementById("promedio").value=promedio;
 
 
 
